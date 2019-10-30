73af9b315de38dfaa3bc956bb32c91bb107075b2
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-10-30 08:00:26 -0700

    Inital failing test

--- RemoveDuplicates/Program.cs
+++ RemoveDuplicates/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace RemoveDuplicates
 {
-    class Program
+      class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+    }
+    public class Answer
+    {
+        public List<int> Elements;
+        public Answer(List<int> inputList)
+        {
+            this.Elements = GetAnswer(inputList);
+        }
+        
+        public List<int> GetAnswer(List<int> inputList)
+        {
+            return null;
         }
     }
 }
--- RemoveDuplicatesTests/UnitTest1.cs
+++ RemoveDuplicatesTests/UnitTest1.cs
@@ -1,14 +1,21 @@
 using System;
 using Xunit;
+using FluentAssertions;
+using System.Collections.Generic;
+using RemoveDuplicates;
 
 namespace RemoveDuplicatesTests
 {
-    public class UnitTest1
+    public class RemoveDuplicatesTests
     {
         [Fact]
         public void Test1()
         {
+            var inputList = new List<int>();
+            var expected = new List<int>();
+            var actual = new Answer(inputList);
 
+            actual.Should().Be(expected);
         }
     }
 }
