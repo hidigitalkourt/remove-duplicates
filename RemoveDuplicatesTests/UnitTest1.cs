9db86634ef9f3c3b68efd37f8983c24fbb05dfe2
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-10-31 07:00:17 -0700

    Failing test for distinct values

--- RemoveDuplicatesTests/UnitTest1.cs
+++ RemoveDuplicatesTests/UnitTest1.cs
@@ -9,10 +9,21 @@ namespace RemoveDuplicatesTests
     public class RemoveDuplicatesTests
     {
         [Fact]
-        public void Test1()
+        public void ReturnEmptyIfEmpty()
         {
             var inputList = new List<int>(){};
             var expected = new List<int>(){};
+            var answer = new Answer(inputList);
+            var actual = answer.Elements;
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ReturnListForDistinctValues()
+        {
+            var inputList = new List<int>(){1,2};
+            var expected = new List<int>(){1,2};
             var answer = new Answer(inputList);
             var actual = answer.Elements;
 
