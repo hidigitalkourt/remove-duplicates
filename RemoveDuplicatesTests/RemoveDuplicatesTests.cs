5d928a19caaf44a77d4ab6894e719f0b52900b4c
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-11-01 12:45:34 -0700

    Update Tests to include use of generics and updated names specific to integer testing

--- RemoveDuplicatesTests/RemoveDuplicatesTests.cs
+++ RemoveDuplicatesTests/RemoveDuplicatesTests.cs
@@ -3,49 +3,52 @@ using Xunit;
 using FluentAssertions;
 using System.Collections.Generic;
 using RemoveDuplicates;
+using static RemoveDuplicates.Program;
 
 namespace RemoveDuplicatesTests
 {
     public class RemoveDuplicatesTests
     {
         [Fact]
-        public void ReturnEmptyIfEmpty()
+        public void ReturnEmptyIfEmptyListIntegers()
         {
             var inputValues = new List<int>(){};
             var expected = new List<int>(){};
-            var actual = Answer.Deduplicate(inputValues);
+            var actual = Answer<int>.Deduplicate(inputValues);
            
             actual.Should().BeEquivalentTo(expected);
         }
 
         [Fact]
-        public void ReturnListForDistinctValues()
+        public void ReturnListForDistinctIntegers()
         {
             var inputValues = new List<int>(){1,2};
             var expected = new List<int>(){1,2};
-            var actual = Answer.Deduplicate(inputValues);
+            var actual = Answer<int>.Deduplicate(inputValues);
            
             actual.Should().BeEquivalentTo(expected);
         }
 
         [Fact]
-        public void ReturnListForNonDistinctValues()
+        public void ReturnListForNonDistinctIntegers()
         {
             var inputValues = new List<int>(){1,1,2,2,3,3};
             var expected = new List<int>(){1,2,3};
-            var actual = Answer.Deduplicate(inputValues);
+            var actual = Answer<int>.Deduplicate(inputValues);
            
             actual.Should().BeEquivalentTo(expected);
         }
 
         [Fact]
-        public void ReturnListForNonDistinctValuesNonOrdered()
+        public void ReturnListForNonDistinctIntegersNonOrdered()
         {
             var inputValues = new List<int>(){1,2,3,1,2,3};
             var expected = new List<int>(){1,2,3};
-            var actual = Answer.Deduplicate(inputValues);
+            var actual = Answer<int>.Deduplicate(inputValues);
            
             actual.Should().BeEquivalentTo(expected);
         }
+
+        
     }
 }
